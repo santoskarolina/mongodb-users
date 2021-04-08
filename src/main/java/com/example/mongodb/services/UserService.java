c071b165a939245097c1afaead94f21f2b4d60bd
ANA KAROLINA SANTOS OLIVEIRA <anna15soliveira@gmail.com>
2021-04-08 08:44:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/example/mongodb/services/UserService.java
+++ src/main/java/com/example/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mongodb.domain.User;
 import com.example.mongodb.repository.UserRepository;
+import com.example.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
